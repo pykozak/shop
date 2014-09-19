d9e137690657d30ebcbc383185cbed174cb3ff79
pykozak <pykozak@gmail.com>
2014-09-19 20:08:48 +0200

    Created Category document

--- src/apps/products/models.py
+++ src/apps/products/models.py
@@ -5,3 +5,8 @@ class Product(mongoengine.Document):
     name = mongoengine.StringField()
     price = mongoengine.DecimalField()
     promotion_price = mongoengine.DecimalField()
+
+
+class Category(mongoengine.Document):
+    name = mongoengine.StringField()
+    products = mongoengine.ListField(mongoengine.ReferenceField(Product))
