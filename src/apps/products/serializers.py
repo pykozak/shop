c1d71fb6cf1cf7f1e2c4fa0a3cd8211144a0f302
pykozak <pykozak@gmail.com>
2014-09-22 22:52:10 +0200

    Created CategorySerializer

--- src/apps/products/serializers.py
+++ src/apps/products/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers as base_serializers
 from rest_framework_mongoengine import serializers
 
 from . import models
@@ -6,3 +7,11 @@ from . import models
 class ProductSerializer(serializers.MongoEngineModelSerializer):
     class Meta:
         model = models.Product
+
+
+class CategorySerializer(serializers.MongoEngineModelSerializer):
+    product_count = base_serializers.Field(source='product_count')
+
+    class Meta:
+        model = models.Category
+        fields = ['id', 'name', 'product_count']
