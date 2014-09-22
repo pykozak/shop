9e4b3a3dfd4a39dc622dbb7cb5b75b0551ced7a8
pykozak <pykozak@gmail.com>
2014-09-22 19:48:56 +0200

    Updated project.urls

--- src/project/urls.py
+++ src/project/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^api/', include('products.api_urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
